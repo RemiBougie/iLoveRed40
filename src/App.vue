897752c74016e766528a6c6d47cf6a8d1dab915d
RemiBougie <remi.bougie@gmail.com>
2025-01-15 15:52:42 -0600

    minor CSS formatting and another attempt at loading background image

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import Banner from './components/Banner.vue'
-import Todos from './components/Todos.vue'
-import UpcomingShows from './components/UpcomingShows.vue'
-import ContactForm from './components/ContactForm.vue'
+import Banner from "./components/Banner.vue";
+import Todos from "./components/Todos.vue";
+import UpcomingShows from "./components/UpcomingShows.vue";
+import ContactForm from "./components/ContactForm.vue";
 // TO-DO: remove everything related to Todos
 </script>
 
 <template>
   <main>
     <Banner />
-    <Todos />
-    <UpcomingShows />
-    <ContactForm />
+    <div class="contents">
+      <UpcomingShows />
+      <ContactForm />
+    </div>
   </main>
 </template>
-
