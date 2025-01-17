99c04592f98a07315d88c3c4ed70f4a6ebece812
RemiBougie <remi.bougie@gmail.com>
2025-01-17 15:27:00 -0600

    set up Authenticator component from aws-amplify/ui-vue. Added toggle buttons and some customizations to the component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+import Authenticator from "./components/Authenticator.vue";
 import Banner from "./components/Banner.vue";
 import Todos from "./components/Todos.vue";
 import UpcomingShows from "./components/UpcomingShows.vue";
 import ContactForm from "./components/ContactForm.vue";
 // TO-DO: remove everything related to Todos
+
+const showAuthenticator = ref(false);
+
+const toggleAuthenticator = () => {
+  showAuthenticator.value = !showAuthenticator.value;
+  //showAuthenticator.value = true;
+};
 </script>
 
 <template>
   <main>
+    <Authenticator v-if="showAuthenticator"/>
+    <button class="admin-login-btn" @click="toggleAuthenticator">Admin Login</button>
     <Banner />
     <div class="contents">
       <UpcomingShows />
@@ -15,3 +27,15 @@ import ContactForm from "./components/ContactForm.vue";
     </div>
   </main>
 </template>
+
+<style>
+
+.admin-login-btn {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: fit-content;
+  margin: 5px;
+}
+
+</style>
---
+++ src/components/Authenticator.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+
+import { Amplify } from "aws-amplify";
+//import outputs from '../amplify_outputs.json';
+
+//Amplify.configure(outputs);
+const showAuthenticator = ref(true);
+
+const toggleAuthenticator = () => {
+  showAuthenticator.value = !showAuthenticator.value;
+}
+</script>
+
+<template>
+  <authenticator :hide-sign-up="true" variation="modal" v-if="showAuthenticator">
+    <template v-slot:sign-in-header>
+      <h3
+        class="amplify-heading"
+        style="padding: var(--amplify-space-xl) 0 0 var(--amplify-space-xl)"
+      >
+        Admin Login
+      </h3>
+    </template>
+
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+
+    <template v-slot:sign-in-footer>
+      <button @click="toggleAuthenticator">Close</button>
+    </template>
+  </authenticator>
+  
+</template>
